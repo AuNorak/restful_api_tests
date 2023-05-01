c7182f4c5f0c1fdbd8ab7768c787006b507d0385
AuNorak <jonfister@gmail.com>
2023-05-01 08:58:28 -0400

    Initial setup and passing tests

---
+++ restful_testful.py
@@ -0,0 +1,20 @@
+import unittest
+import requests
+
+class TestRestfulAPI(unittest.TestCase):
+    BASE_URL = "https://restful-api.dev/"
+
+    def test_get(self):
+        pass  # Implement GET test here
+
+    def test_post(self):
+        pass  # Implement POST test here
+
+    def test_put(self):
+        pass  # Implement PUT test here
+
+    def test_delete(self):
+        pass  # Implement DELETE test here
+
+if __name__ == "__main__":
+    unittest.main()
