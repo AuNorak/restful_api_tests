190aee4c0ab85e60b0d54ae42faf98deef94bba9
AuNorak <jonfister@gmail.com>
2023-05-15 05:03:23 -0400

    Use objects fixture in PUT tests and clean up imports

--- test/api/objects/constants.py
+++ test/api/objects/constants.py
@@ -1,4 +1,3 @@
-BASE_URL = "https://api.restful-api.dev"
 DEFAULT_PAYLOAD = {"name": "Test Product", "data": {"color": "blue", "generation": "1st", "price": 99}}
 headers = {"content-type": "application/json"}
 request_url = "https://api.restful-api.dev/objects"
--- test/api/objects/test_PUT_restful.py
+++ test/api/objects/test_PUT_restful.py
@@ -1,19 +1,12 @@
 import pytest
 import requests
-import json
-from constants import BASE_URL, DEFAULT_PAYLOAD, headers, request_url
+from constants import headers, request_url
 
-def POST_create_new_data(payload=DEFAULT_PAYLOAD):
-    response = requests.post(request_url, data=json.dumps(payload), headers=headers)
-    created_object = response.json()
-    return created_object
-
-def test_put_update_existing_object_name():
-    created_object_id = POST_create_new_data()["id"]
-    created_object_endpoint = f"{request_url}/{created_object_id}"
+def test_put_update_existing_object_name(objects_fixture):
+    created_object_endpoint = f"{request_url}/{objects_fixture}"
 
     put_payload = {"name": "Apple AirPods", "data": {"color": "white", "generation": "3rd", "price": 120}}
-    put_response = requests.put(created_object_endpoint, data=json.dumps(put_payload), headers=headers)
+    put_response = requests.put(created_object_endpoint, json=put_payload, headers=headers)
     assert put_response.status_code == 200
     print(f"PUT content: {put_response.content}")
 
@@ -22,36 +15,33 @@ def test_put_update_existing_object_name():
     fetched_object = get_response.json()
     assert fetched_object["data"]["price"] == 120
 
-def test_put_update_non_existent_object():
+def test_put_update_non_existent_object(objects_fixture):
     non_existent_id = 9999
     put_endpoint = f"{request_url}/{non_existent_id}"
 
     put_payload = {"name": "Apple AirPods", "data": {"color": "white", "generation": "3rd", "price": 120}}
-    put_response = requests.put(put_endpoint, data=json.dumps(put_payload), headers=headers)
+    put_response = requests.put(put_endpoint, json=put_payload, headers=headers)
     print(f"PUT content: {put_response.content}")
     assert put_response.status_code == 404
 
 # The follow tests fail because REST API accepts PUT requests that are malformed, if this is acceptable the assertions could be changed or the tests removed
 
-def test_put_invalid_data_type():
-    created_object_id = POST_create_new_data()["id"]
-    created_object_endpoint = f"{request_url}/{created_object_id}"
+def test_put_invalid_data_type(objects_fixture):
+    created_object_endpoint = f"{request_url}/{objects_fixture}"
 
     put_payload = {"name": "Test Product", "data": {"color": "blue", "price": "invalid"}}
-    put_response = requests.put(created_object_endpoint, data=json.dumps(put_payload), headers=headers)
+    put_response = requests.put(created_object_endpoint, json=put_payload, headers=headers)
     assert put_response.status_code == 400
 
-def test_put_extra_field():
-    created_object_id = POST_create_new_data()["id"]
-    created_object_endpoint = f"{request_url}/{created_object_id}"
+def test_put_extra_field(objects_fixture):
+    created_object_endpoint = f"{request_url}/{objects_fixture}"
 
     put_payload = {"name": "Test Product", "extra_field": "Not required", "data": {"color": "blue", "price": 99}}
-    put_response = requests.put(created_object_endpoint, data=json.dumps(put_payload), headers=headers)
+    put_response = requests.put(created_object_endpoint, json=put_payload, headers=headers)
     assert put_response.status_code == 400
 
-def test_put_empty_json():
-    created_object_id = POST_create_new_data()["id"]
-    created_object_endpoint = f"{request_url}/{created_object_id}"
+def test_put_empty_json(objects_fixture):
+    created_object_endpoint = f"{request_url}/{objects_fixture}"
 
     put_response = requests.post(created_object_endpoint, json={}, headers=headers)
     assert put_response.status_code == 400
