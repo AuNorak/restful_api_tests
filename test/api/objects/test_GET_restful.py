627da43ffd16eab83bf2198625fb305ea6097c47
AuNorak <jonfister@gmail.com>
2023-05-15 05:04:22 -0400

    Cleanup GET tests, imports and expand shape test

--- test/api/objects/test_GET_restful.py
+++ test/api/objects/test_GET_restful.py
@@ -1,37 +1,32 @@
 import requests
+from constants import request_url
 
 class TestRestfulAPI():
-    BASE_URL = "https://api.restful-api.dev"
-
     def test_get_all_objects_returns_200(self):
-        endpoint = f"{self.BASE_URL}/objects"
-        response = requests.get(endpoint)
+        response = requests.get(request_url)
         assert response.status_code == 200
-    # tests the endpoint returns 200 for the full list of objects
 
     # def test_get_all_objects_as_unauthenticated_returns_403(self):
-    #     endpoint = f"{self.BASE_URL}/objects/42"
+    #     endpoint = f"{request_url}/42"
     #     response = requests.get(endpoint)
     #     self.assertEqual(response.status_code, 403)
     # tests the endpoint returns 403 for a user without permission making get request
 
     def test_get_single_object_returns_200(self):
-        endpoint = f"{self.BASE_URL}/objects/1"
+        endpoint = f"{request_url}/1"
         response = requests.get(endpoint)
         assert response.status_code == 200
-    # tests the endpoint returns 200 for a single objects in the list
 
     def test_get_multiple_object_returns_requested_objects(self):
-        endpoint = f"{self.BASE_URL}/objects?id=1&id=4&id=5"
+        endpoint = f"{request_url}?id=1&id=4&id=5"
         response = requests.get(endpoint)
         data = response.json()
         expected_ids = ["1", "4", "5"]
         actual_ids = [obj['id'] for obj in data]
         assert actual_ids == expected_ids
-    # tests the endpoint returns 3 objects and the correct objects
 
     def test_get_multiple_same_object_returns_multiple_objects(self):
-        endpoint = f"{self.BASE_URL}/objects?id=1&id=1&id=1"
+        endpoint = f"{request_url}?id=1&id=1&id=1"
         response = requests.get(endpoint)
         data = response.json()
         expected_ids = ["1","1","1"]
@@ -41,25 +36,24 @@ class TestRestfulAPI():
     # was looking for a breaking case, there may be an argument to remove this
 
     def test_get_all_objects_returns_list_of_objects(self):
-        endpoint = f"{self.BASE_URL}/objects"
-        response = requests.get(endpoint)
+        response = requests.get(request_url)
         data = response.json()
         assert isinstance(data, list)
         for obj in data:
             assert isinstance(obj, dict)
-    # tests the endpoint returns 200 for the full list of objects
 
     def test_get_single_object_shape(self):
-        endpoint = f"{self.BASE_URL}/objects/1"
+        endpoint = f"{request_url}/1"
         response = requests.get(endpoint)
         data = response.json()
         expected_keys = ['id', 'name', 'data']
-        # assert sorted(data.keys()) == sorted(expected_keys)
         assert set(data.keys()) == expected_keys
-    # tests the shape of the object is vaguely as expected
+        assert isinstance(data.id, int)
+        assert isinstance(data.name, str)
+        assert isinstance(data.data, dict)
 
     def test_get_string_object_returns_404_with_error_payload(self):
-        endpoint = f"{self.BASE_URL}/objects/jon"
+        endpoint = f"{request_url}/jon"
         response = requests.get(endpoint)
         payload = {'error': 'Object with id=jon was not found.'}
         assert response.status_code == 404
@@ -69,7 +63,6 @@ class TestRestfulAPI():
     # test fails, but this is a bug in restful API so I'm leaving the assertion as is
 
     def test_get_non_existent_object_returns_404(self):
-        endpoint = f"{self.BASE_URL}/objects/100"
+        endpoint = f"{request_url}/100"
         response = requests.get(endpoint)
         assert response.status_code == 404
-    # tests the endpoint returns 404 for a single object not in the list
